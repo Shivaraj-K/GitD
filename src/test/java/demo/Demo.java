abe52f680546762d33cb8c6ad3655bb91b2a6351
Shivaraj-K <kshivu741@gmail.com>
2023-06-17 22:17:15 +0530

    added

---
+++ src/test/java/demo/Demo.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void demotest()
+	{
+		System.out.println("Demo111");
+	}
+	@Test
+	public void demotest1()
+	{
+		System.out.println("Dem");
+	}
+}
